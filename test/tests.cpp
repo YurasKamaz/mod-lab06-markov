075517d59935a7195a7118b6d1832e470eb42dce
YurasKamaz <tyertyn@gmail.com>
2024-04-19 19:29:49 +0300

    test fix v4

--- test/tests.cpp
+++ test/tests.cpp
@@ -4,21 +4,21 @@
 
 TEST(test1, PrefixFormation) {
     Generator generator;
-    generator.train("the quick brown fox jumps over the lazy dog", 2);
+    generator.Train("the quick brown fox jumps over the lazy dog", 2);
     Prefix prefix = {"the", "quick"};
     EXPECT_EQ(generator.getChain()->first, prefix);
 }
 
 TEST(test2, CheckSuffix) {
     Generator generator;
-    generator.train("the quick brown fox jumps over the lazy dog", 2);
+    generator.Train("the quick brown fox jumps over the lazy dog", 2);
     Prefix prefix = {"the", "quick"};
     EXPECT_EQ(generator.getChain().at(prefix)[0], "brown");
 }
 
 TEST(test3, SingleSuffixSelection) {
     Generator generator;
-    generator.train("the quick brown fox jumps over the lazy dog", 2);
+    generator.Train("the quick brown fox jumps over the lazy dog", 2);
     std::string gen = generator.Generate(100);
     EXPECT_EQ(gen, "brown fox jumps over the lazy dog");
 }
