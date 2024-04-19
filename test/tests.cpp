87c6e5a26adb39db17d338f6c19c6a03f0a30473
YurasKamaz <tyertyn@gmail.com>
2024-04-19 19:24:50 +0300

    test fix v2

--- test/tests.cpp
+++ test/tests.cpp
@@ -12,16 +12,19 @@ class MarkovChainTest : public ::testing::Test {
 };
 
 TEST(MarkovChainTest, PrefixFormation) {
+    Generator generator;
     Prefix prefix = {"the", "quick"};
     EXPECT_EQ(generator.getChain()->first, prefix);
 }
 
 TEST(MarkovChainTest, CheckSuffix) {
+    Generator generator;
     Prefix prefix = {"the", "quick"};
     EXPECT_EQ(generator.getChain().at(prefix)[0], "brown");
 }
 
 TEST(MarkovChainTest, SingleSuffixSelection) {
+    Generator generator;
     std::string gen = generator.Generate(100);
     EXPECT_EQ(gen, "brown fox jumps over the lazy dog");
 }
@@ -29,6 +32,6 @@ TEST(MarkovChainTest, SingleSuffixSelection) {
 TEST(MarkovChainTest, MultiplySuffixSelection) {
     Generator generator2;
     generator2.Train("the quick brown fox quick jumps over the lazy dog", 2);
-    std::string gen = generator.Generate(100);
+    std::string gen = generator2.Generate(100);
     EXPECT_EQ(gen, "brown fox jumps over the lazy dog");
 }
